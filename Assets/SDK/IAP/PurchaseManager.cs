2d270d3d71725ab3129774b2ccc4e32898bcfc32
vc <victorzhou3d@gmail.com>
2019-07-08 19:18:56 +0800

    no message

---
+++ Assets/SDK/IAP/PurchaseManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Purchasing;
+
+public class PurchaseManager : MonoBehaviour, IStoreListener
+{
+    public BuyManager buyManager;
+    private IStoreController controller;
+
+    public void Instance()
+    {
+        var module = StandardPurchasingModule.Instance();
+        ConfigurationBuilder builder = ConfigurationBuilder.Instance(module);
+        builder.AddProduct("id0", ProductType.NonConsumable);
+        builder.AddProduct("id1", ProductType.Subscription);
+        builder.AddProduct("Id2", ProductType.Consumable);
+        UnityPurchasing.Initialize(this, builder);
+    }
+
+   
+
+    private void OnInitialized(IStoreController controller, IExtensionProvider extensions)
+    {
+        this.controller = controller;
+    }
+    private void OnInitializeFailed(InitializationFailureReason error)
+    {
+
+    }
+    private void OnPurchaseFailed(Product i, PurchaseFailureReason p)
+    {
+
+    }
+    private PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs e)
+    {
+        return PurchaseProcessingResult.Complete;
+    }
+
+    public void OnPurchaseClicked(string productId)
+    {
+        controller.InitiatePurchase(productId);
+    }
+}
---
+++ Assets/Scripts/01Basics/05Common/Web/WebRequestManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+
+/// <summary>
+/// Http Web 请求
+/// </summary>
+public class WebRequestManager : MonoBehaviour
+{
+
+    public IEnumerator Get(string path, Action<string> action)
+    {
+        if (!string.IsNullOrEmpty(path))
+        {
+            UnityWebRequest www = new UnityWebRequest(path);
+            yield return www.SendWebRequest();
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.LogError(www.error);
+            }
+            else
+            {
+                Debug.Log(www.downloadHandler.text);
+                string text = www.downloadHandler.text;
+                action?.Invoke(text);
+            }
+        }
+        yield return null;
+    }
+
+    public IEnumerator Post(string path, List<IMultipartFormSection> formData)
+    {
+        if (!string.IsNullOrEmpty(path) && formData != null)
+        {
+            UnityWebRequest www = UnityWebRequest.Post(path, formData);
+            yield return www.SendWebRequest();
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+            }
+        }
+        yield return null;
+    }
+
+
+    public IEnumerator Post(string path, WWWForm form)
+    {
+        if (!string.IsNullOrEmpty(path) && form != null)
+        {
+            UnityWebRequest www = UnityWebRequest.Post(path, form);
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+            }
+        }
+        yield return null;
+    }
+
+
+    public IEnumerator Download()
+    {
+        yield return null;
+
+    }
+
+    public IEnumerator Upload(string path)
+    {
+        yield return null;
+    }
+}
